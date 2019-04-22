e2c3433b65e0a1cbaf699b8c0364a879e346694f
kenkuts <kennethchoi19@gmail.com>
2019-04-22 01:40:30 -0400

    sleep

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,19 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: :show
+
   def index
     @posts = Post.all
   end
   
+  def show
+  end
+  
+  def new 
+  end
+  
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
